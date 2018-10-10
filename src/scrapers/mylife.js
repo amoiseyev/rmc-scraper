50e52dc39145ff59b0d867cbda3eac663fecd1d3
Bogdan Kozlov <bkozlow@waverleysoftware.com>
2018-10-10 18:24:56 +0300

    * Rework MyLife scraper * Add downloadFile to utils

--- src/scrapers/mylife.js
+++ src/scrapers/mylife.js
@@ -1,32 +1,76 @@
+import fs from 'fs'
+import xmlParse from 'xml-parser'
+import { ungzip } from 'node-gzip'
 import Nightmare from 'nightmare'
-import { removeNode, calculateAge } from './utils'
-import { getNightmareConfig } from './constants'
+
+import { downloadFile } from './utils'
+import { getNightmareConfig, appPath } from './constants'
+
+const sitemapXmlFile = 'people_sitemapindex1.xml'
+const gzTmpFilePath = `${appPath}/tmp/data.xml.gz`
+const xmlFilePath = `${appPath}/tmp/${sitemapXmlFile}`
 
 class MyLife {
-    static run (queryData) {
+    static getSiteMapChunks (sid) {
+        return downloadFile(`${sid}/${sitemapXmlFile}`, xmlFilePath).then(() => {
+            const xml = fs.readFileSync(xmlFilePath, 'utf8')
+            return xmlParse(xml)
+        }).then((result) => {
+            fs.unlinkSync(xmlFilePath)
+
+            const siteMapChunks = result.root.children
+
+            return siteMapChunks.map((siteMapChunk) => {
+                const siteMapChunkLink = siteMapChunk.children[0].content
+                return siteMapChunkLink
+            })
+        })
+    }
+
+    static getDataEntryPoints = async (sid) => {
+        const chunksLinks = await MyLife.getSiteMapChunks(sid)
+
+        return downloadFile(chunksLinks[0], gzTmpFilePath).then(() => {
+            const compressed = fs.readFileSync(gzTmpFilePath)
+
+            return ungzip(compressed).then((decompressed) => {
+                return xmlParse(decompressed.toString())
+            })
+        }).then((result) => {
+            fs.unlinkSync(gzTmpFilePath)
+
+            const peopleLinks = result.root.children
+
+            return peopleLinks.map((link) => {
+                return link.children[0].content
+            })
+        })
+    }
+
+    static run = async (queryData) => {
+        const dataLinks = await MyLife.getDataEntryPoints(queryData.sid)
+
         const nightmare = Nightmare(getNightmareConfig())
 
+        console.log(dataLinks[0])
+
         nightmare
-            .goto(queryData.sid)
-            .type('input[name="searchFirstName"]', queryData.firstName)
-            .type('input[name="searchLastName"]', queryData.lastName)
-            .type('input[name="searchLocation"]', queryData.city)
-            .click('.nameSearchSubmit')
-            .wait('#searchAge')
-            .type('#searchAge', calculateAge(new Date(queryData.birthday)))
-            .evaluate(removeNode, '.text-capitalize') // to capture .text-capitalize via wait after page load
-            .click('#identity-claim-form-multi .btn.btn-primary')
-            .wait('.text-capitalize')
-            .evaluate(() => {
-                const list = document.querySelectorAll('.row.well-plain')
-                const results = []
+            .goto(dataLinks[0])
+            .evaluate((link) => {
+                const nameAgeLine = document.querySelector('.profile-information-name-age').innerText
+                const name = nameAgeLine.split(',')[0].split(' ')
+                const location = document.querySelector('.profile-information-location').innerText.split(',')
+                const personData = {}
 
-                list.forEach((item) => {
-                    results.push(item.innerText)
-                })
+                personData.id = link
+                personData.firstName = name[0]
+                personData.lastName = name[name.length - 1]
+                personData.age = nameAgeLine.split(',')[1].trim()
+                personData.city = location[0]
+                personData.state = location[1].trim()
 
-                return results
-            })
+                return personData
+            }, dataLinks[0])
             .end()
             .then((results) => {
                 console.log('result: ', results)
--- src/scrapers/utils.js
+++ src/scrapers/utils.js
@@ -1,9 +1,28 @@
+import http from 'http'
+import https from 'https'
+import fs from 'fs'
+
 export const removeNode = (selector) => document.querySelector(selector).remove()
 
 export const calculateAge = function (birthday) {
     // birthday is a date
-    var ageDifMs = Date.now() - birthday.getTime()
-    var ageDate = new Date(ageDifMs) // miliseconds from epoch
+    const ageDifMs = Date.now() - birthday.getTime()
+    const ageDate = new Date(ageDifMs) // miliseconds from epoch
 
     return Math.abs(ageDate.getUTCFullYear() - 1970)
+}
+
+export const downloadFile = function (url, dest) {
+    const file = fs.createWriteStream(dest)
+    const protocol = url.includes('https') ? https : http
+
+    return new Promise((resolve, reject) => protocol.get(url, function (response) {
+        response.pipe(file)
+        file.on('finish', function () {
+            file.close(resolve) // close() is async, call cb after close completes.
+        })
+    }).on('error', function (err) { // Handle errors
+        fs.unlinkSync(dest) // Delete the file async. (But we don't check the result)
+        reject(err.message)
+    }))
 }
